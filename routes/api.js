28862ad0f5b024f59a261a337fc319890d1a36dd
Obilarius <git@walzenbach.com>
2019-10-16 15:58:58 +0200

    after Lec14 (REST API Tut - Net Ninja)

---
+++ routes/api.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const Ninja = require("../models/ninja")
+
+// get a list of ninjas from the db
+router.get("/ninjas", (req, res, next) => {
+  // Ninja.find()
+  //   .then((ninjas) => {
+  //     res.status(200).send(ninjas);
+  //   })
+  //   .catch(next);
+
+  Ninja.aggregate().near({
+    near: {
+      'type': 'Point',
+      'coordinates': [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+    },
+    maxDistance: 100000,
+    spherical: true,
+    distanceField: "dis"
+  }).then((ninjas) => {
+    res.status(200).send(ninjas);
+  }).catch(next);
+});
+
+// add a new ninja to the db
+router.post("/ninjas", (req, res, next) => {
+  Ninja.create(req.body).then(ninja => {
+    res.status(200).send(ninja);
+  }).catch(next);
+});
+
+// update a ninja in the db
+router.put("/ninjas/:id", (req, res, next) => {
+  Ninja.findByIdAndUpdate({
+      _id: req.params.id
+    }, req.body)
+    .then((ninja) => {
+      Ninja.findById(req.params.id).then((ninja) => {
+        res.status(200).send(ninja)
+      })
+    })
+    .catch(next);
+});
+
+// delete a ninja in the db
+router.delete("/ninjas/:id", (req, res, next) => {
+  Ninja.findByIdAndRemove({
+      _id: req.params.id
+    })
+    .then((ninja) => {
+      res.status(200).send(ninja)
+    })
+    .catch(next);
+});
+
+module.exports = router;
