ce3f08a8ffce373a8317d74eb61f541b1f47eaae
Obilarius <git@walzenbach.com>
2019-10-18 10:06:38 +0200

    NetNinja Kurs beendet

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // set up express app
 const app = express();
 
 // connect to mongoDB
-mongoose.connect("mongodb+srv://apiuser:Q1qWJoUWrnwexfOp@themovielib-7zind.mongodb.net/test?retryWrites=true&w=majority", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true
-})
+mongoose.connect(
+	"mongodb+srv://apiuser:Q1qWJoUWrnwexfOp@themovielib-7zind.mongodb.net/test?retryWrites=true&w=majority", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	}
+);
 mongoose.Promise = global.Promise;
 
+// CORS
+app.use(cors());
+
+app.use(express.static("public"));
+
 app.use(bodyParser.json());
+
 // init routes
 app.use("/api", require("./routes/api"));
+
 // error handling middleware
 app.use((err, req, res, next) => {
 	res.status(400).send({
 		error: err.message
-	})
-})
-
+	});
+});
 
 // port
 const port = process.env.port || 4000;
